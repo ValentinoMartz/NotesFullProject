064f57d6d9bb830b7ca0520170aab013f408c518
ValentinoMartz <valentinomartinez55@gmail.com>
2022-09-06 17:01:07 -0300

    back working...

---
+++ api/src/routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+/* const videogames = require("./videogames");
+const videogame = require("./videogame");
+const genres = require("./genres");
+
+router.use("/videogames", videogames);
+router.use("/videogame", videogame);
+router.use("/genres", genres); */
+
+module.exports = router;
