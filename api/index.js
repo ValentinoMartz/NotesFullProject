5d8b0aab3abf2c911922442c40efd8bfdef8f6a4
ValentinoMartz <valentinomartinez55@gmail.com>
2022-09-07 19:43:35 -0300

    details

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ const { conn } = require("./src/db.js");
   });
 }); */
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,15 +2,10 @@ const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
+const axios = require("axios");
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-/* const videogames = require("./videogames");
-const videogame = require("./videogame");
-const genres = require("./genres");
-
-router.use("/videogames", videogames);
-router.use("/videogame", videogame);
-router.use("/genres", genres); */
+router.get("/test", async (req, res) => {
+  res.status(200).send("El hombre lombriz le va a partir la cara a V** M**");
+});
 
 module.exports = router;
