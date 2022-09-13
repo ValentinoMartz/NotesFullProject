49cf699cbb609d0b2c0dd6ed8ef49c258f6970e8
ValentinoMartz <valentinomartinez55@gmail.com>
2022-09-13 19:37:52 -0300

    mui instalado

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Typography from "@mui/material/Typography";
+import TagFacesIcon from "@mui/icons-material/TagFaces";
+import { Box } from "@mui/material";
 
 function App() {
-  return <div className="App">Hola</div>;
+  return (
+    <Box>
+      <Typography variant="h1" textAlign="center">
+        Hola
+      </Typography>
+      <TagFacesIcon alignItem="center" justifyContent="center" />
+    </Box>
+  );
 }
 
 export default App;
